0e3cebf6a78b1366eb492fc6cd9de47898add2b5
jisoo-log <jisoolee.dev@gmail.com>
2020-12-07 23:57:10 +0900

    Delete unused components

--- App.js
+++ App.js
@@ -1,13 +1,5 @@
 import React, { useState } from "react";
-import {
-  StyleSheet,
-  Text,
-  TextInput,
-  View,
-  Button,
-  ScrollView,
-  FlatList,
-} from "react-native";
+import { StyleSheet, View, FlatList } from "react-native";
 import GoalItem from "./components/GoalItem";
 import GoalInput from "./components/GoalInput";
 
@@ -25,7 +17,6 @@ export default function App() {
     <View style={styles.screen}>
       <GoalInput addGoalHandler={addGoalHandler} />
       <FlatList
-        keyExtractor={(item, index) => item.key}
         data={courseGoals}
         renderItem={(itemData) => (
           <GoalItem title={itemData.item.value}></GoalItem>
